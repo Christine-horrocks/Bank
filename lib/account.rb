ace567bbfe92d9f91b942eb1982918ce75ddad57
Christine <splogy2001@hotmail.com>
2017-11-27 16:38:19 +0000

    Added balance variable and chnaged balance on transactions

--- lib/account.rb
+++ lib/account.rb
@@ -1,23 +1,29 @@
 class Account
-
   attr_reader :transactions
+  attr_accessor :balance
 
   def initialize
     @transactions = []
+    @balance = 0
   end
 
-  def deposit(money, date = Time.now.strftime("%d/%m/%y"))
-    deposit = {date: date, type: 'deposit', amount: money}
+  def deposit(money, date = Time.now.strftime('%d / %m / %y'))
+    @balance += money
+    deposit = { date: date, credit: money, debit: ' ', balance: @balance }
     @transactions << deposit
   end
 
-  def withdraw(money, date = Time.now.strftime("%d/%m/%y"))
-    withdrawal = {date: date, type: 'withdrawal', amount: - money}
+  def withdraw(money, date = Time.now.strftime('%d / %m / %y'))
+    @balance -= money
+    withdrawal = { date: date, credit: ' ', debit: money, balance: @balance }
     @transactions << withdrawal
   end
 
   def print_statement
-    return "date || credit || debit || balance \n 27/ 11/ 17|| 300 || || 300 \n 27/ 11/ 17|| || 100 || 200"
+    puts 'date || credit || debit || balance'
+    @transactions.each do |t|
+      puts "#{t[:date]} || #{t[:credit]} || #{t[:debit]} || #{t[:balance]}"
+    end
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,19 +6,25 @@ describe Account do
     expect(subject.transactions).to be_an_instance_of(Array)
   end
 
-  it 'can make a deposit to the account' do
-    subject.deposit(100, 10/10/10)
-    expect(subject.transactions).to eq([{date: 10/10/10, type: 'deposit', amount: 100}])
+  it 'can make a deposit' do
+    subject.deposit(100, 10 / 10 / 10)
+    expect(subject.transactions).to eq([{ date: 10 / 10 / 10, credit: 100, debit: ' ', balance: 100 }])
   end
 
-  it 'can make a withdrawal on the account' do
-    subject.withdraw(100, 10/10/10)
-    expect(subject.transactions).to eq([{date: 10/10/10, type: 'withdrawal', amount: -100}])
+  it 'can make a withdrawal' do
+    subject.withdraw(100, 10 / 10 / 10)
+    expect(subject.transactions).to eq([{ date: 10 / 10 / 10, credit: ' ', debit: 100, balance: -100 }])
   end
 
-  it 'can print out the statement' do
-    subject.deposit(300)
-    subject.withdraw(100)
-    expect(subject.print_statement).to eq("date || credit || debit || balance \n 27/ 11/ 17|| 300 || || 300 \n 27/ 11/ 17|| || 100 || 200" )
+  it 'can change the balance when more than one transaction takes place' do
+    subject.deposit(300, 10 / 10 / 10)
+    subject.withdraw(100, 10 / 10 / 10)
+    expect(subject.transactions).to eq([{ date: 10 / 10 / 10, credit: 300, debit: ' ', balance: 300 }, { date: 10 / 10 / 10, credit: ' ', debit: 100, balance: 200 }])
   end
+
+  # it 'can print out the statement' do
+  #   subject.deposit(300, 10 / 10 / 10)
+  #   subject.withdraw(100, 10 / 10 / 10)
+  #   expect(subject.print_statement).to eq("date || credit || debit || balance \n 10/10/10|| 300 || || 300 \n 10/10/10|| || 100 || 200")
+  # end
 end
